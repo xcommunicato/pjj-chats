5a902f9a069c6276fc57cb9c1a34cf2404024836
xcommunicato <xcommunicato@gmail.com>
2015-08-10 10:10:45 -0500

    Update index.php

--- v2/index.php
+++ v2/index.php
@@ -11,6 +11,8 @@
 <html>
 <head>
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<meta name="Author" content="Tino Didriksen <mail@tinodidriksen.com>">
 	<meta name="Generator" content="Tino Didriksen <mail@tinodidriksen.com>">
 	<meta name="google-site-verification" content="dDCWHjXgRUCemnG36GYPWNtUIa0V_5FL2nC4J4jAB9g" />
@@ -25,6 +27,9 @@ if (window != window.top) {
 </script>
 
 <style>
+/*** Generic "Reset" Styling ***/
+html, body, div, span, applet, object, iframe, h1, h2, h3, h4, h5, h6, p, blockquote, pre, a, abbr, acronym, address,  big, cite, code, del, dfn, em, img, ins, kbd, q, s, samp, small, strike, strong, sub, sup, tt, var, b, u, i, center,  dl, dt, dd, ol, ul, li, fieldset, form, label, legend, table, caption, tbody, tfoot, thead, tr, th, td, article,  aside, canvas, details, embed, figure, figcaption, footer, header, hgroup, menu, nav, output, ruby, section, summary,  time, mark, audio, video { margin: 0; padding: 0; border: 0; font-size: 100%; font: inherit; vertical-align: baseline;  } article, aside, details, figcaption, figure, footer, header, hgroup, menu, nav, section { display: block; } body {  line-height: 1; } ol, ul { list-style: none; } blockquote, q { quotes: none; } blockquote:before, blockquote:after,  q:before, q:after { content: ''; content: none; } table { border-collapse: collapse; border-spacing: 0; } * { margin: 0; } html, body { height: 100%; } body { background-color: #000; font-family: Arial, Helvetica Neue, serif; } a { transition: all .3s ease; }
+
 body {
     margin: 0;
     padding: 0;
@@ -46,7 +51,7 @@ a:hover {
 
 <body>
 <table width="100%" border="0" cellspacing="0" cellpadding="0">
-<tr><td valign="top" align="left" height="32" style="background-image: URL('http://pjj.cc/gfx/up_tile.gif'); background-repeat: repeat-x;">
+<tr><td valign="top" align="left" height="32" style="background-image: URL('//pjj.cc/gfx/up_tile.gif'); background-repeat: repeat-x;">
 	<table width="100%" border="0" cellspacing="0" cellpadding="0" height="32">
 	<tr><td valign="top" align="left"width="151"><a href="http://projectjj.com/"><img src="gfx/projectjj2.gif" border="0"></a></td>
 	<td valign="top" align="right" width="151"><a href="http://projectjj.com/" target=_blank><img src="gfx/phpchat2.gif" border="0"></a></td></tr>
